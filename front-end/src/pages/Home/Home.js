1e706f1330c66cbd24a760663c80c657b652c37f
minsu-zip <alstn4814@gmail.com>
2020-10-17 23:51:44 +0900

    [박민수] HomePage 수정

--- front-end/src/pages/Home/Home.js
+++ front-end/src/pages/Home/Home.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React from "react";
 
 // 메인 페이지 (만든 설문을 보는 페이지)
 const Home = () => {
-    return(
-        <>
-        HOME
-        </>
-    )
-}
+  return (
+    <>
+      HOME
+      <table>
+        <tr>
+            <td></td>
+          <th>월</th>
+          <th>화</th>
+          <th>수</th>
+        </tr>
+        <tr>
+            <td>1교시</td>
+            <td>국어</td>
+            <td>영어</td>
+            <td>수학</td>
+        </tr>
+        <tr>
+            <td>2교시</td>
+        </tr>
+        <tr>
+            <td>3교시</td>
+        </tr>
+      </table>
+    </>
+  );
+};
 
 export default Home;
