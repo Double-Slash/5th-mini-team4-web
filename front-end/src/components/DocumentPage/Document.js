d886a87a5d4679c4af8cb01f966888b01dba4998
minsu-zip <alstn4814@gmail.com>
2020-12-27 16:41:44 +0900

    [ADD] Document Page 생성_박민수

---
+++ front-end/src/components/DocumentPage/Document.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Document = () => {
+  return (
+    <>
+      <div className="container">
+          <h3>개인용</h3>
+      </div>
+    </>
+  );
+};
+
+export default Document;
