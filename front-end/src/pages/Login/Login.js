7c801d9dfd9e870d60175cc36d7a95bed2074de6
Cho Seong week <tjddnr0217@gmail.com>
2020-12-29 16:49:36 +0900

    fix: 로그인 페이지 수정 & 로그인 css 수정

--- front-end/src/pages/Login/Login.js
+++ front-end/src/pages/Login/Login.js
@@ -1,22 +1,29 @@
 import React, { useState } from 'react';
 import './Login.css'
 
-import arrowright from '../../images/arrowright.svg';
 import checkbox from '../../images/checkbox.svg';
 import checked from '../../images/checked.svg';
-import info from '../../images/info.svg'
-import google from '../../images/google.svg'
+import google from '../../images/google.svg';
+import view from '../../images/view.svg';
+import viewOff from '../../images/viewOff.svg';
+
+import { View32, ViewOff32, ArrowRight32, Information32 } from "@carbon/icons-react";
 
 function Login() {
   const [remember, setRemember] = useState(false);
+  const [viewOn, setViewOn ] = useState(false);
 
   const onRemember = () => {
     setRemember(!remember);
   }
 
+  const onViewOn = () => {
+    setViewOn(!viewOn);
+  }
+
   return (
-    <div className='loginBack'>
-      <div style={{ position: 'relative', left: 180, top:72, width: 464}}>
+    <div className='Background'>
+      <div className='Container'>
         
         <div style={{ fontSize: 32, fontWeight: 'bold'}}>Log in</div>
         
@@ -29,65 +36,50 @@ function Login() {
 
         <div style={{ display: 'flex', justifyContent: 'space-between', margin: '14px 0 14px 0'}}>
           <div className='infoLabel'>Continue with Assessment id</div>
-          <div style={{ fontSize: 12, color: '#08bdba'}}>Forgot ID?</div>
+          <div style={{ fontSize: 12, color: '#08bdba', cursor: 'pointer'}}>Forgot ID?</div>
         </div>
 
         <div style={{ display: 'flex', flexDirection: 'column'}}>
           <input 
             className='loginInput'
             placeholder='userEmail@Email.com'/>
-          
-          <div style={{ marginTop: 16 }}/>
-          
-          <div>
+                    
+          <div className='passwordContatiner'>
             <input 
               type='password'
               className='passwordInput'
               placeholder='Password'/>
+            <img onClick={onViewOn} src={viewOn ? view : viewOff} style={{ width: 32, height: 32}} alt="remember"/>
           </div>
-
-          <div style={{ marginTop: 16 }}/>
 
           <div className='loginButton'>
             <div style={{ fontSize: 20, color: 'white'}}>Continue</div>
-            <img src={arrowright} style={{ width: 32, height: 32}}/>
+            <ArrowRight32 />
           </div>
         </div>
         
-        <div style={{ marginTop: 24 }}/>
-
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <img onClick={onRemember} src={remember ? checked : checkbox} style={{ width: 32, height: 32}}/>
+        <div style={{ display: 'flex', alignItems: 'center', marginTop: 24 }}>
+          <img onClick={onRemember} src={remember ? checked : checkbox} style={{ width: 32, height: 32}} alt="remember"/>
           
-          <div style={{ marginLeft: 8 }}/>
+          <div style={{ fontSize: 16, marginLeft: 8, marginRight: 20 }}>Remeber ID</div>
           
-          <div style={{ fontSize: 16 }}>Remeber ID</div>
-
-          <div style={{ marginLeft: 20 }}/>
-          
-          <img src={info} style={{ width: 24, height: 24}}/>
+          <Information32 />
         </div>
 
         <div style={{ border:'1px solid #f2f4f8', marginTop: 42 }} />
 
-        <div style={{ marginTop: 15 }}/>
-
-        <div>
+        <div style={{ marginTop: 15 }}>
           <div className='infoLabel'>Alternative logins</div>
-          
-          <div style={{ marginTop: 14 }}/>
-          
+                    
           <div className='googleButton'>
             <div style={{ fontSize: 20, color: '#08bdba' }}>Log in with Google</div>
-            <img src={google} style={{ width: 32, height: 32 }} />
+            <img src={google} style={{ width: 32, height: 32 }} alt="google"/>
           </div>
         </div>
 
         <div style={{ border:'1px solid #f2f4f8', marginTop: 31 }} />
 
-        <div style={{ marginTop: 17 }}/>
-
-        <div style={{ display: 'flex' }}>
+        <div style={{ display: 'flex', marginTop: 17 }}>
           <div className='infoLabel'>Need help?</div>
           <div className='helpButton'>Contact the Assessement help desk</div>
         </div>
