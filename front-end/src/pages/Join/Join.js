f2308a27e24f37cb4180265852bc234daf1be042
Cho Seong week <tjddnr0217@gmail.com>
2020-12-31 15:51:12 +0900

    refactor: join 페이지 backgroun 이미지 추가 및 css 리팩토링, Join.js 리팩토링

--- front-end/src/pages/Join/Join.js
+++ front-end/src/pages/Join/Join.js
@@ -1,90 +1,132 @@
 import React, { useState } from 'react';
 import './Join.css'
 
-import arrowright from '../../images/arrowright.svg';
 import checkbox from '../../images/checkbox.svg';
 import checked from '../../images/checked.svg';
-import info from '../../images/info.svg'
 
-function Join() {
-    const [remember, setRemember] = useState(false);
+import { ArrowRight32, Information32 } from "@carbon/icons-react";
+
+function Join({ history }) {
+  const [remember, setRemember] = useState(false);
+  const [inputs, setInputs] = useState({
+    userId: '',
+    password: '',
+    passwordCheck: '',
+    name: '',
+    phone: '',
+  })
+
+  const onChange = (e) => {
+    const { value, name } = e.target
+    setInputs({
+      ...inputs,
+      [name] : value
+    })
+  }
   
-    const onRemember = () => {
-      setRemember(!remember);
+  const onRemember = () => {
+    setRemember(!remember);
+  }
+
+  const onSignUp = () => {
+    if(remember){
+      localStorage.setItem('userId',inputs.userId);
     }
+    if(inputs.password === inputs.passwordCheck){
+      history.push('/login');
+    }
+    else{
+      alert("비밀번호가 다릅니다. 확인해주세요!");
+    }
+  }
   
-    return (
-
-      <div className='joinBack'>
-        <div className='join-wrapper'>
-        <div style={{ position: 'relative', top:72, width: 464}}>
+  return (
+    <div className='joinBack'>
+      <div className='join-wrapper'>
+        <div className="joinContainer">
+            
           <div style={{ border:'1px solid #dde1e6', marginTop: 49}} /> 
-  
-          <div style={{ display: 'flex', flexDirection: 'column'}}>
-          <div style={{ marginTop: 17}}/>
-            <div style={{ color: '#696969' }}>사용할 이메일</div>
-            <div style={{ marginTop: 10}}/>
-            <input 
-              className='idInput'
-              placeholder='userEmail@Email.com'/>
-
-            <div style={{ display: 'flex', marginTop: 5 }}>
-             <img onClick={onRemember} src={remember ? checked : checkbox} style={{ width: 32, height: 32}}/>
-
-             <div style={{ marginLeft: 8 }}/>
-             <div style={{ fontSize: 16, marginTop: 5 }}>Remeber ID</div>
-             <div style={{ marginLeft: 10 }}/>
-
-             <img src={info} style={{ width: 24, height: 24, marginTop: 5 }}/>
-            </div>
-            
-            <div style={{ border:'1px solid #dde1e6', marginTop: 30}} /> 
-            <div style={{ marginTop: 16 }}/>
-            
-            <div style={{ color: '#696969' }}>비밀번호</div> 
-            <div style={{ marginTop: 10}}/>
-            <div>
-              <input 
-                type='password'
-                className='passwordInput'
-                placeholder='Password'/>
-            </div>
-
-            <div style={{ marginTop: 16 }}/>
-
-            <div style={{ color: '#696969' }}>비밀번호 확인</div>
     
-            <div style={{ marginLeft: 10 }}/>
+          <div style={{ display: 'flex', flexDirection: 'column'}}>
 
             <div>
-            <input 
-              type='password'
-              className='passwordCheckInput'
-              placeholder='Password check'/>
-           </div>
-
-           <div style={{marginTop: 16}}/>
-
-           <div style={{ color: '#696969' }}>이름</div>
-           <div style={{ marginTop: 10}}/>
-           <div>
-             <input
-              type='nickname'
-              className='nameInput'
-              placeholder='Name ex)홍길동'/>
-           </div>
-
-           <div style={{ marginTop: 34 }}/>
-  
-           <div className='joinButton'>
-           <div style={{ fontSize: 20, color: 'white'}}>Continue</div>
-            <img src={arrowright} style={{ width: 32, height: 32, marginLeft: 300}}/>
+              <div style={{ color: '#696969', marginTop: 17 }}>사용할 이메일</div>
+              <input 
+                name='userId'
+                value={inputs.userId}
+                onChange={onChange}
+                className='joinInput'
+                placeholder='userEmail@Email.com'/>
             </div>
+
+            <div style={{ display: 'flex', alignItems: 'center', marginTop: 24 }}>
+              <img onClick={onRemember} src={remember ? checked : checkbox} style={{ width: 32, height: 32}} alt="remember"/>
+              
+              <div style={{ fontSize: 16, marginLeft: 8, marginRight: 20 }}>Remeber ID</div>
+              
+              <Information32 style={{ color: '#dde1e6'}}/>
+            </div>
+              
+              <div style={{ border:'1px solid #dde1e6', marginTop: 30}} /> 
+              
+            <div>
+              <div style={{ color: '#696969', marginTop: 16 }}>비밀번호</div> 
+              <div style={{ marginTop: 10}}/>
+              <div>
+                <input 
+                  name='password'
+                  value={inputs.password}
+                  onChange={onChange}
+                  type='password'
+                  className='joinInput'
+                  placeholder='Password ex) 1234abcd'/>
+              </div>
+            </div>
+            
+            <div>
+              <div style={{ color: '#696969', marginTop: 16 }}>비밀번호 확인</div>
+      
+              <div style={{ marginLeft: 10 }}/>
+
+              <input
+                name='passwordCheck'
+                value={inputs.passwordCheck}
+                onChange={onChange}
+                type='password'
+                className='joinInput'
+                placeholder='Password check'/>
+            </div>
+            
+            <div>
+              <div style={{ color: '#696969', marginTop: 16 }}>이름</div>
+              <input
+                name='name'
+                value={inputs.name}
+                onChange={onChange}
+                type='nickname'
+                className='joinInput'
+                placeholder='Name ex)홍길동'/>
+            </div>
+
+            <div>
+              <div style={{ color: '#696969', marginTop: 16 }}>전화번호</div>
+              <input
+                name='phone'
+                value={inputs.phone}
+                onChange={onChange}
+                className='joinInput'
+                placeholder='082-010-1234-5678'/>
+            </div>
+    
+            <div className='joinButton' onClick={onSignUp}>
+              <div style={{ fontSize: 20, color: 'white'}}>Continue</div>
+              <ArrowRight32 style={{ color: '#ffffff'}}/>
+            </div>
+          </div>
         </div>
+      </div> 
     </div>
-    </div> 
-    </div>
-    );
+  );
 }
 
 export default Join;
