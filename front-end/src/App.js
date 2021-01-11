d885fb365611a71e2dee29e01aa61d3e8acbbc6e
minsu-zip <alstn4814@gmail.com>
2021-01-11 14:35:07 +0900

    [UPDATE] package update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,30 +1,36 @@
-import React from 'react';
-import {HashRouter, Route} from 'react-router-dom';
-import Home from './pages/Home/Home'
-import Assessment from './components/Assessment/Assessment';
-import Category from './components/Category/Category';
-import Navigation from './pages/Navigation';
-import Header from './pages/Header';
-import CreateAssessment from'./pages/EditAssess/CreateAssessment';
-import Main from './pages/Main/Main'
-import Login from './pages/Login/Login'
-import Join from './pages/Join/Join'
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Assessment from "./components/Assessment/Assessment";
+import Category from "./components/Category/Category";
+import Navigation from "./pages/Navigation";
+import Header from "./pages/Header";
+import CreateAssessment from "./pages/EditAssess/CreateAssessment";
+import Main from "./pages/Main/Main";
+import Login from "./pages/Login/Login";
+import Join from "./pages/Join/Join";
+import document from "./components/DocumentPage/Document";
 
 function App() {
   return (
     <>
-    <Header></Header>
-    <HashRouter>
-      {/* Navigation은 개발 완료시 삭제예정 개발시 페이지 참고용 */}
-      <Navigation></Navigation>
-      <Route path="/" exact={true} component={Home}></Route>
-      <Route path='/login' exact={true} component={Login} />
-      <Route path='/join' exact={true} component={Join} />
-      <Route path="/category" exact={true} component={Category}></Route>
-      <Route path="/assessment" exact={true} component={Assessment}></Route>
-      <Route path="/createassessment" exact={true} component={CreateAssessment} />
-      <Route path="/main" exact={true} component={Main} />
-    </HashRouter>
+      <Header></Header>
+      <HashRouter>
+        {/* Navigation은 개발 완료시 삭제예정 개발시 페이지 참고용 */}
+        <Navigation></Navigation>
+        <Route path="/" exact={true} component={Home}></Route>
+        <Route path="/login" exact={true} component={Login} />
+        <Route path="/join" exact={true} component={Join} />
+        <Route path="/category" exact={true} component={Category}></Route>
+        <Route path="/assessment" exact={true} component={Assessment}></Route>
+        <Route
+          path="/createassessment"
+          exact={true}
+          component={CreateAssessment}
+        />
+        <Route path="/document" exact={true} component={document} />
+        <Route path="/main" exact={true} component={Main} />
+      </HashRouter>
     </>
   );
 }
--- front-end/src/pages/Navigation.js
+++ front-end/src/pages/Navigation.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import "../css/Navigation.css";
 
 // Navbar 최종시 없앨예정 개발시 페이지 구성 편의를 위해
 const Navigation = () => {
-    return(
-        <>
-        <div className="nav">
-            <Link to="/">HOME</Link>
-            <Link to="/assessment">Assessment</Link>
-            <Link to="/createassessment">createassessment</Link>
-            <Link to="/document">document</Link>
-        </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="nav">
+        <Link to="/">HOME</Link>
+        <Link to="/assessment">Assessment</Link>
+        <Link to="/createassessment">createassessment</Link>
+        <Link to="/document">document</Link>
+      </div>
+    </>
+  );
+};
 
 export default Navigation;
