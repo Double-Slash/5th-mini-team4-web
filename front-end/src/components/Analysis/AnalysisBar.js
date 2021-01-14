fe588f687d3bb2e1c4aba54fa00d975bdb75c178
Cho Seong week <tjddnr0217@gmail.com>
2021-01-14 16:35:59 +0900

    feat: 분석페이지 제작 - 제목, NavBar

--- front-end/src/components/Analysis/AnalysisBar.js
+++ front-end/src/components/Analysis/AnalysisBar.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function AnalysisBar() {
+function AnalysisBar(props) {
   return (
-    <div>
-      NavBar 입니다.
-    </div>
+    <>
+      <div style={{ display: 'flex', justifyContent:'space-between'}}>
+        <div>
+          <LineMenu 
+            items={props.leftMenu}
+            leftSelect={props.leftSelect}
+            onMenuClick={props.onMenuClick}
+          />
+        </div>
+        <div>
+          <LineMenu 
+            items={props.rightMenu}
+            rightSelect={props.rightSelect}
+            onMenuClick={props.onMenuClick} 
+          />
+        </div>
+      </div>
+    </>
   );
+}
+
+function LineMenu({ 
+  items, 
+  onMenuClick, 
+  rightSelect, 
+  leftSelect 
+}) {
+  return (
+    <div style={{ display: 'flex'}}>
+      {
+        items.map((item,index) => {
+          return(   
+            <div
+              key={index} 
+              onClick={() => onMenuClick(item.text)}
+              style={{ 
+                padding: '12px 147px 12px 16px',
+                borderBottom: leftSelect === item.text || rightSelect === item.text ? '2px solid #08bdba' : null,
+                fontWeight: leftSelect === item.text || rightSelect === item.text ? 'bold': null
+            }}>{item.text}</div>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default AnalysisBar;
--- front-end/src/components/Analysis/AnalysisList.js
+++ front-end/src/components/Analysis/AnalysisList.js
@@ -1,13 +1,57 @@
-import React, { userState } from 'react';
+import React, { useState } from 'react';
 
 import AnalysisHeader from './AnalysisHeader';
 import AnalysisBar from './AnalysisBar'
 
-function AnalysisList() {
+import { Download32, Search32, Settings32 } from "@carbon/icons-react";
+
+function AnalysisList(props) {
   return (
     <div>
       <AnalysisHeader />
-      <AnalysisBar />
+      
+      <div style={{ marginTop: 72, padding: '0 64px' }}>
+        <AnalysisBar 
+          leftMenu={props.leftMenu}
+          rightMenu={props.rightMenu}
+          leftSelect={props.leftSelect}
+          rightSelect={props.rightSelect}
+          onMenuClick={props.onMenuClick}
+        />
+       
+        <div style={{ backgroundColor: '#f2f4f8' }}>
+          <div style={{ fontSize: 32, fontWeight: 'bold', padding: '8px 16px 0 16px'}}>
+            기술통계 - {props.rightSelect}
+          </div>
+          
+          <div style={{ color: '#878d96', fontSize: 24, padding: '0 16px' }}>
+            숫자 입력응답을 분석한 표
+          </div>
+          
+          <div style={{ 
+            display:'flex',
+            justifyContent:'space-between',
+            alignItems: 'center',
+            marginTop: 20
+            }}>
+            <div style={{ 
+              display:'flex',
+              justifyContent:'space-between',
+              backgroundColor: '#08bdba',
+              alignItems: 'center',
+              padding: '6px 16px' }}>
+              <div style={{ color: 'white', fontSize: 24 }}>다운로드</div>
+              <Download32 style={{ color: 'white', marginLeft: 90 }}/>
+
+            </div>
+            <div>
+              <Search32 />
+              <Settings32 style={{marginLeft: 32, marginRight: 16 }}/>
+            </div>
+          </div>
+        </div>
+        
+      </div>
     </div>
   );
 }
